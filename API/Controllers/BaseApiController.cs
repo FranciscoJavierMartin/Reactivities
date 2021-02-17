076a3555aa04e7125ce57b1a77a2907c5b172ff9
Francisco <javiermartincuenca@gmail.com>
2021-02-17 10:59:31 +0100

    Error handling on backend

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,7 +12,30 @@ namespace API.Controllers
   {
     private IMediator _mediator;
 
-    protected IMediator Mediator => 
+    protected IMediator Mediator =>
       _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+      ActionResult res;
+      if (result == null)
+      {
+        res = NotFound();
+      }
+      else if (result.IsSuccess && result.Value != null)
+      {
+        res = Ok(result.Value);
+      }
+      else if (result.IsSuccess && result.Value == null)
+      {
+        res = NotFound();
+      }
+      else
+      {
+        res = BadRequest(result.Error);
+      }
+
+      return res;
+    }
   }
 }
